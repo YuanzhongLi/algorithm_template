39d6132a8da4830e0e599a65bef5f9b4d2b26704
YuanzhongLi <liyuanzhongutokyos1@gmail.com>
2020-06-01 20:58:11 +0900

    update FLOW

--- FLOW/最小費用流/PrimalDual.cpp
+++ FLOW/最小費用流/PrimalDual.cpp
@@ -1,16 +1,66 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-#define rep(i,s,n) for (int i = (int)s; i < (int)n; i++)
+#define rep(i,s,n) for (int i = (ll)s; i < (ll)n; i++)
+#define rrep(i,n,e) for (int i = (ll)n-1; i >= (ll)e; i--)
 #define ll long long
+#define ld long double
 #define pb push_back
+#define eb emplace_back
 #define All(x) x.begin(), x.end()
 #define Range(x, i, j) x.begin() + i, x.begin() + j
 #define lbidx(x, y) lower_bound(x.begin(), x.end(), y) - x.begin()
 #define ubidx(x, y) upper_bound(x.begin(), x.end(), y) - x.begin()
-#define BiSearchRangeNum(x, y, z) lower_bound(x.begin(), x.end(), z) - lower_bound(x.begin(), x.end(), y)
-#define deg_to_rad(deg) ((((double)deg)/((double)360)*2*M_PI))
-#define rad_to_deg(rad) ((((double)rad)/(double)2/M_PI)*(double)360)
+#define llbidx(x, y, z) lower_bound(x.begin(), x.end(), z) - lower_bound(x.begin(), x.end(), y) // 二要素間の距離
+// #define M_PI 3.14159265358979323846 // CFでは定義しておく必要あり
+#define deg2rad(deg) ((((double)deg)/((double)360)*2*M_PI))
+#define rad2deg(rad) ((((double)rad)/(double)2/M_PI)*(double)360)
+#define Find(set, element) set.find(element) != set.end()
+#define Decimal(x) cout << fixed << setprecision(10) << x << endl; // 小数点を10桁まで表示
+// debug用
+#define PrintVec(x) for (auto elementPrintVec: x) { cout << elementPrintVec << " "; } cout << "\n";
+#define debug(x) cerr << #x << ": " << (x) << "\n";
+#define endl "\n"
+// gcj print用
+#define Case(x) printf("Case #%d: ", x);
+
+typedef pair<int, int> PI;
+typedef pair<ll, ll> PLL;
+typedef vector<int> vi;
+typedef vector<vector<int>> vvi;
+typedef vector<vector<vector<int>>> vvvi;
+typedef vector<ll> vl;
+typedef vector<vector<ll>> vvl;
+typedef vector<vector<vector<int>>> vvvl;
+typedef vector<PI> vpi;
+typedef vector<vector<PI>> vvpi;
+typedef vector<vector<vector<PI>>> vvvpi;
+typedef vector<PLL> vpl;
+typedef vector<vector<PLL>> vvpl;
+typedef vector<vector<vector<PLL>>> vvvpl;
+
+const ll LINF = 1001002003004005006ll;
+const int INF = 1001001001;
+
+int POWINT(int x, int n) {
+  int ret = 1;
+  while (n > 0) {
+    if (n & 1) ret *= x;
+    x *= x;
+    n >>= 1;
+  }
+  return ret;
+};
+
+ll POWLL(ll x, int n) {
+  ll ret = 1;
+  while (n > 0) {
+    if (n & 1) ret *= x;
+    x *= x;
+    n >>= 1;
+  }
+  return ret;
+};
 
 template<class T>
 inline bool chmax(T &a, T b) {
@@ -30,34 +80,48 @@ inline bool chmin(T &a, T b) {
   return false;
 };
 
+void print() {
+  cout << endl;
+};
+
+template <class T>
+void print(vector<T> &vec) {
+  for (auto& a : vec) {
+    cout << a;
+    if (&a != &vec.back()) cout << " ";
+  }
+  cout << endl;
+};
+
+// VERIFICATIOIN: utpc2012_J
+// URL: https://atcoder.jp/contests/utpc2012/submissions/13638244
+
 // PrimalDual法
 /*
+ある始点から終点へと流量Fを流す時の最小費用を求める。
 最小費用流を最短路反復で解くアルゴリズム。始点から終点までの重みの最短路を求め、そこに流せる限り流す。これを流したい分だけ流しきるまで繰り返す。
 最短路の計算は、ポテンシャル h を用いて負辺がないように変換して Dijkstra法 で求める。
 */
 
 // 計算量 O(F * E * logV)
-
-static const int INF = (1<<30);
-
-struct PrimalDual {
+class PrimalDual {
   struct edge {
     int to, cap, cost, rev;
   };
-
+public:
   vector<vector<edge>> G;
   vector<int> potential, min_cost;
   vector<int> prevv, preve;
 
   PrimalDual(int V): G(V) {}
 
-  void add_edge(int from ,int to, int cap, int cost) {
+  void addEdge(int from ,int to, int cap, int cost) {
     G[from].pb((edge) { to, cap, cost, (int) G[to].size() });
     // capそのままでcostをマイナスにする
     G[to].pb((edge) { from, 0, -cost, (int) G[from].size()-1 });
   }
 
-  int min_cost_flow(int s, int t, int f) {
+  int minCostFlow(int s, int t, int f) {
     int V = (int) G.size();
     int ret = 0;
     // <cost, node番号>
@@ -118,18 +182,4 @@ struct PrimalDual {
 
     return ret;
   }
-};
-
-int main() {
-  int V, E, F;
-  cin >> V >> E >> F;
-  int u, v, c, d;
-  struct PrimalDual pd(V);
-  rep(i, 0, E) {
-    cin >> u >> v >> c >> d;
-    pd.add_edge(u, v, c, d);
-  }
-
-  int ans = pd.min_cost_flow(0, V - 1, F);
-  cout << ans << endl;
 };
