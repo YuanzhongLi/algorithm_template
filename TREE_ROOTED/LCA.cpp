5b415d9e0c4fa7b6b1be35a683a9decf63d88b22
YuanzhongLi <liyuanzhongutokyos1@gmail.com>
2020-06-17 23:20:29 +0900

    update Tree rooted && add LCA

---
+++ TREE_ROOTED/LCA.cpp
@@ -0,0 +1,91 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+/* LCA
+ VERIFICATION: HackerRank_ysf1_G
+ URL: https://www.hackerrank.com/contests/ysf1/challenges/ysf1-g/submissions/code/1324331490
+*/
+
+// <to, dist>
+vector<pair<int, int>> G[200005];
+class LCA {
+public:
+  int V, logV; // V: node num
+  vector<int> depth, dist;
+  vector<vector<int> > parent;
+
+  LCA(int V) {
+    this->V = V;
+    logV = 0;
+    while (V > (1LL<<logV)) logV++;
+    this->depth = vector<int>(V);
+    this->dist = vector<int>(V);
+    this->parent = vector<vector<int> >(logV, vector<int>(V));
+  }
+
+  void init(int v, int par, int d, int l) { // dfs: calculate dist and parent
+    depth[v] = d;
+    parent[0][v] = par;
+    dist[v] = l;
+    for (int i = 0; i < (int)G[v].size(); i++) {
+      int w = G[v][i].first;
+      int lc = G[v][i].second;
+      if (w == par) continue;
+      init(w, v, d+1, lc + l);
+    }
+  }
+
+  void build() { // doubling: build parent
+    for (int k = 0; k + 1 < logV; k++) {
+      for (int v = 0; v < V; v++) {
+        if (parent[k][v] < 0) parent[k+1][v] = -1;
+        else parent[k+1][v] = parent[k][parent[k][v]];
+      }
+    }
+  }
+
+  int query(int u, int v) {
+    if (depth[u] > depth[v]) swap(u, v);
+    for (int k = 0; k < logV; k++) {
+      if ((depth[v] - depth[u]) >> k & 1) v = parent[k][v];
+    }
+    if (u == v) return u;
+
+    for (int k = logV-1; k >= 0; k--) {
+      if (parent[k][u] != parent[k][v]) {
+        u = parent[k][u];
+        v = parent[k][v];
+      }
+    }
+    return parent[0][u];
+  }
+
+  int getDistance(int u, int v) {
+    int z = query(u, v);
+    return dist[u] + dist[v] - 2 * dist[z];
+  }
+};
+
+int main() {
+  int N; cin >> N;
+
+  for (int i = 0; i < N-1; i++) {
+    int x, y, d; cin >> x >> y >> d;
+    x--; y--;
+    G[x].push_back(make_pair(y, d));
+    G[y].push_back(make_pair(x, d));
+  }
+
+  LCA lca(N);
+  lca.init(0, -1, 0, 0);
+  lca.build();
+
+  int Q; cin >> Q;
+  while (Q--) {
+    int x, y; cin >> x >> y;
+    x--; y--;
+    cout << lca.getDistance(x, y) << endl;
+  }
+
+  return 0;
+};
---
+++ TREE_ROOTED/ancestor.cpp
@@ -0,0 +1,94 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define rep(i,s,n) for (int i = (int)s; i < (int)n; i++)
+#define ll long long
+#define ld long double
+#define pb push_back
+#define eb emplace_back
+#define All(x) x.begin(), x.end()
+#define Range(x, i, j) x.begin() + i, x.begin() + j
+#define lbidx(x, y) lower_bound(x.begin(), x.end(), y) - x.begin()
+#define ubidx(x, y) upper_bound(x.begin(), x.end(), y) - x.begin()
+#define llbidx(x, y, z) lower_bound(x.begin(), x.end(), z) - lower_bound(x.begin(), x.end(), y) // 二要素間の距離
+#define deg2rad(deg) ((((double)deg)/((double)360)*2*M_PI))
+#define rad2deg(rad) ((((double)rad)/(double)2/M_PI)*(double)360)
+#define Find(set, element) set.find(element) != set.end()
+#define Decimal(x) printf("%.10f\n", x) // 小数点を10桁まで表示
+// debug用
+#define PrintVec(x) for (auto elementPrintVec: x) { cout << elementPrintVec << " "; } cout << endl;
+#define debug(x) cerr << #x << ": " << (x) << "\n";
+
+typedef pair<int, int> PI;
+typedef pair<ll, ll> PLL;
+typedef vector<int> vi;
+typedef vector<vector<int>> vvi;
+typedef vector<vector<vector<int>>> vvvi;
+typedef vector<ll> vl;
+typedef vector<vector<ll>> vvl;
+typedef vector<vector<vector<int>>> vvvl;
+typedef vector<PI> vpi;
+typedef vector<vector<PI>> vvpi;
+typedef vector<vector<vector<PI>>> vvvpi;
+typedef vector<PLL> vpl;
+typedef vector<vector<PLL>> vvpl;
+typedef vector<vector<vector<PLL>>> vvvpl;
+
+int POWINT(int x, int n) {
+  int ret = 1;
+  rep(i, 0, n) ret *= x;
+  return ret;
+};
+
+ll POWLL(ll x, int n) {
+  ll ret = 1;
+  rep(i, 0, n) ret *= x;
+  return ret;
+};
+
+template<class T>
+inline bool chmax(T &a, T b) {
+  if(a < b) {
+    a = b;
+    return true;
+  }
+  return false;
+};
+
+template<class T>
+inline bool chmin(T &a, T b) {
+  if(a > b) {
+    a = b;
+    return true;
+  }
+  return false;
+};
+
+int T;
+vi par, dist;
+vi tin, tout;
+vvi adj;
+
+void dfs(int u, int p = -1, int dep = 0) {
+  par[u] = p;
+  dist[u] = dep;
+  tin[u] = T++;
+  for (int v: adj[u]) {
+    if (v == p) continue;
+    dfs(v, u, dep+1);
+  }
+  tout[u] = T++;
+};
+
+// if u is v's ancestor
+bool isAnc(int u, int v) {
+  return tin[u] <= tin[v] && tout[v] <= tout[u];
+};
+
+
+int main() {
+  ios::sync_with_stdio(false);
+  cin.tie(0);
+
+  return 0;
+};
