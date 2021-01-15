07a4cd0171d75304ad5c4efa6d3963532f33bc0b
YuanzhongLi <liyuanzhongutokyos1@gmail.com>
2021-01-15 23:16:02 +0900

    add DivideAndConquer

---
+++ DivideAndConquer/ClosestPair.cpp
@@ -0,0 +1,153 @@
+#define LOCAL
+#ifdef LOCAL
+#define _GLIBCXX_DEBUG
+#endif
+#include <bits/stdc++.h>
+using namespace std;
+
+#define rep(i,s,n) for (int i = (ll)s; i < (ll)n; i++)
+#define rrep(i,n,e) for (int i = (ll)n; i > (ll)e; i--)
+#define ll long long
+#define ld long double
+#define pb push_back
+#define eb emplace_back
+#define All(x) x.begin(), x.end()
+#define Range(x, i, j) x.begin() + i, x.begin() + j
+// #define M_PI 3.14159265358979323846 // CF
+#define deg2rad(deg) ((((double)deg)/((double)360)*2*M_PI))
+#define rad2deg(rad) ((((double)rad)/(double)2/M_PI)*(double)360)
+#define Find(set, element) set.find(element) != set.end()
+#define Decimal(x) cout << fixed << setprecision(10) << x << endl; // print Decimal number 10 Rank
+#define endl "\n"
+#define Case(x) printf("Case #%d: ", x); // gcj
+
+typedef pair<int, int> PI;
+typedef pair<ll, ll> PLL;
+typedef vector<int> vi;
+typedef vector<vector<int>> vvi;
+typedef vector<vector<vector<int>>> vvvi;
+typedef vector<ll> vl;
+typedef vector<vector<ll>> vvl;
+typedef vector<vector<vector<ll>>> vvvl;
+typedef vector<PI> vpi;
+typedef vector<vector<PI>> vvpi;
+typedef vector<PLL> vpl;
+typedef vector<vector<PLL>> vvpl;
+typedef vector<char> vch;
+typedef vector<vector<char>> vvch;
+
+constexpr ll LINF = 1001002003004005006ll;
+constexpr int INF = 1002003004;
+constexpr int n_max = 2e5+10;
+
+template<class T>
+inline bool chmax(T &a, T b) { if(a<b) { a=b; return true; } return false; };
+template<class T>
+inline bool chmin(T &a, T b) { if(a>b) { a=b; return true; } return false; };
+
+template<class T, class U>
+T POW(T x, U n) {T ret=1; while (n>0) {if (n&1) {ret*=x;} x*=x; n>>=1;} return ret;};
+
+// debug
+template <typename A, typename B>
+string to_string(pair<A, B> p);
+string to_string(const string &s) {return '"' + s + '"';};
+string to_string(const char c) {return to_string((string) &c);};
+string to_string(bool b) {return (b ? "true" : "false");};
+template <size_t N>
+string to_string(bitset<N> v){
+  string res = "";
+  for(size_t i = 0; i < N; i++) res += static_cast<char>('0' + v[i]);
+  return res;
+};
+template <typename A>
+string to_string(A v) {
+  bool first = true;
+  string res = "{";
+  for(const auto &x : v) {
+    if(!first) res += ", ";
+    first = false; res += to_string(x);
+  }
+  res += "}";
+  return res;
+};
+template <typename A, typename B>
+string to_string(pair<A, B> p){return "(" + to_string(p.first) + ", " + to_string(p.second) + ")";}
+
+void debug_out() {cerr << endl;};
+template<typename Head, typename... Tail>
+void debug_out(Head H, Tail... T) { cerr << " " << to_string(H); debug_out(T...); };
+
+void LINE_OUT() {
+  cout << "--------------" << endl;
+};
+
+#ifdef LOCAL
+#define debug(...) cerr << "[" << #__VA_ARGS__ << "]:", debug_out(__VA_ARGS__)
+#define LINE LINE_OUT();
+#else
+#define debug(...) 71
+#define LINE 71;
+#endif
+
+void print() { cout << endl; }
+template <class Head, class... Tail>
+void print(Head&& head, Tail&&... tail) {
+  cout << head;
+  if (sizeof...(tail) != 0) cout << " ";
+  print(forward<Tail>(tail)...);
+};
+
+template <class T>
+void print(vector<T> &vec) {
+  for (auto& a : vec) {
+    cout << a;
+    if (&a != &vec.back()) cout << " ";
+  }
+  cout << endl;
+};
+
+template <class T>
+void print(vector<vector<T>> &df) {
+  for (auto& vec : df) {
+    print(vec);
+  }
+};
+
+// VERIFICATIOIN: ABC 022_D
+// URL: https://atcoder.jp/contests/abc022/submissions/19447015
+
+// 分割統治法を使用して最近2点対を求める
+// 点の個数をNとするとO(NlogN)
+// dによる制約で分割の間は最大で6個の頂点間のみの探索
+typedef pair<ld, ld> P;
+bool compare_y(P a, P b) {
+  return a.second < b.second;
+};
+// (s, e]
+ld closest_pair(vector<P> &A, int s, int e) {
+  int n = e-s;
+  if (n <= 1) return (ld)LINF;
+  int m = n/2;
+  ld x = A[s+m].first;
+  ld d = min<ld>(closest_pair(A, s, s+m), closest_pair(A, s+m, e)); // divide and recurrent
+  inplace_merge(A.begin()+s, A.begin()+s+m, A.begin()+e, compare_y); // merge 2 sorted array
+
+  // now A is y ascending order order
+
+  vector<P> B; // store nodes which distance is less than d
+  for (int i = 0; i < n; i++) {
+    if (fabsl(A[s+i].first - x) >= d) continue;
+
+    // see b's node from tail to diff with y is more than d
+    for (int j = 0; j < B.size(); j++) {
+      ld dx = A[s+i].first - B[B.size() - j - 1].first;
+      ld dy = A[s+i].second - B[B.size() - j - 1].second;
+      if (dy >= d) break;
+      d = min<ld>(d, sqrtl(dx*dx+dy*dy));
+    }
+    B.push_back(A[s+i]);
+  }
+
+  return d;
+};
